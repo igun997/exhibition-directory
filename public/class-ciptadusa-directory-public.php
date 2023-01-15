95e1558318ce255fa63e90a4179dd3a4defd9e2b
indra <indra.gunanda@dev.nusatek.id>
2023-01-16 01:03:57 +0700

    added : complete plugins ready to install

--- public/class-ciptadusa-directory-public.php
+++ public/class-ciptadusa-directory-public.php
@@ -114,7 +114,7 @@ class Ciptadusa_Directory_Public {
 
 		wp_print_script_tag( [
 			'id'    => 'main-exhibitor',
-			'src'   => plugin_dir_url( __FILE__ ) . 'js/main.bb33cff6.js',
+			'src'   => plugin_dir_url( __FILE__ ) . 'js/main.912d754b.js',
 			'defer' => true,
 		] );
 	}
