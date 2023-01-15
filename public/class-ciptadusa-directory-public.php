1cab3aa1c5a2b1b9fc03bb9b6bc5420cf2cba8cb
indra <indra.gunanda@dev.nusatek.id>
2023-01-16 05:50:27 +0700

    added : 1.2.0

--- public/class-ciptadusa-directory-public.php
+++ public/class-ciptadusa-directory-public.php
@@ -114,7 +114,7 @@ class Ciptadusa_Directory_Public {
 
 		wp_print_script_tag( [
 			'id'    => 'main-exhibitor',
-			'src'   => plugin_dir_url( __FILE__ ) . 'js/main.84a1a6a3.js',
+			'src'   => plugin_dir_url( __FILE__ ) . 'js/main.b3f7c9c5.js',
 			'defer' => true,
 		] );
 	}
