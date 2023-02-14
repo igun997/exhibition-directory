39b7e140e946b308af6b672af39754fb24cd93d2
indra <indra.gunanda@dev.nusatek.id>
2023-02-14 16:22:23 +0700

    remove sidebar limit

--- public/class-ciptadusa-directory-public.php
+++ public/class-ciptadusa-directory-public.php
@@ -114,7 +114,7 @@ class Ciptadusa_Directory_Public {
 
 		wp_print_script_tag( [
 			'id'    => 'main-exhibitor',
-			'src'   => plugin_dir_url( __FILE__ ) . 'js/main.f9eda321.js',
+			'src'   => plugin_dir_url( __FILE__ ) . 'js/main.5dd2594b.js',
 			'defer' => true,
 		] );
 	}
