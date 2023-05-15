2db53cd215ed8bc736f4d94b205707685f3e6d4c
indra <indra.gunanda@dev.nusatek.id>
2023-05-15 18:00:41 +0700

    added : v1.7.1

--- public/class-ciptadusa-directory-public.php
+++ public/class-ciptadusa-directory-public.php
@@ -113,7 +113,7 @@ class Ciptadusa_Directory_Public {
 		] );
 		wp_print_script_tag( [
 			'id'    => 'main-exhibitor',
-			'src'   => plugin_dir_url( __FILE__ ) . 'js/main.8912c504.js',
+			'src'   => plugin_dir_url( __FILE__ ) . 'js/main.77eac337.js',
 			'defer' => true,
 		] );
 	}
